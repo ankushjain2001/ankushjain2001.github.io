2458b7f832484e56941b1bc2e681d7ed14c61fce
Ankush Jain <ankushjain2001@yahoo.co.in>
2021-01-11 14:47:06 -0600

    Half completed

--- js/data.js
+++ js/data.js
@@ -1,21 +1,70 @@
 var data = {
   "about": {
-    "intro": "Hello! I am Ankush Jain. I am a machine learning engineer and a full-stack developer. My experience has taught me to apply my skills to real-world problems. And my on-going study in the master's program at <a href=\"https://engineering.nyu.edu/academics/programs/computer-science-ms\" class=\"text-primary\">New York University</a> has further equipped me with all the essential tools for my niche.<br><br>Previously, I have worked as a Software Engineer at <a href=\"https://www.infosys.com/services/validation-solutions/service-offerings/machine-learning-qa.html\" class=\"text-primary\">Infosys</a>. I worked on a variety of machine learning solutions for Software Quality Assurance & Testing. Presently, I am working part-time with <a href=\" https://vyorius.com/\" class=\"text-primary\">Vyorius</a> to deploy their monolith applications on AWS.",
-    "end": "Here are my best skills in <span class=\"about-ml-underline\">machine learning</span> and <span class=\"about-fs-underline\">full-stack dev</span> :",
-    "techs": "<li>Python</li>\
-              <li>PyTorch / Keras</li>\
-              <li>Spark / Hadoop</li>\
-              <li>Natural Language Processing</li>\
-              <li>Transformer Models</li>\
-              <li>Unsupervised Learning</li>\
-              ",
-    "skills": "<li>HTML / CSS / JS</li>\
-              <li>SQL / MongoDB</li>\
-              <li>Node.js / Flask</li>\
-              <li>React / JQuery / AngularJS</li>\
-              <li>Amazon Web Services</li>\
-              <li>Elasticsearch</li>\
-              ",
- 
-  }
+    "intro": "Hello! I am Ankush Jain. I am a machine learning engineer and a full-stack developer. My experience has taught me to apply my skills to real-world problems. And my on-going study in the master's program at <a href=\"https://engineering.nyu.edu/academics/programs/computer-science-ms\" class=\"text-primary\">New York University</a> has further equipped me with all the essential tools for my niche.<br><br>Previously, I have worked as a Software Engineer at <a href=\"https://www.infosys.com/services/validation-solutions/service-offerings/machine-learning-qa.html\" class=\"text-primary\">Infosys</a>. I worked on a variety of machine learning solutions for Software Quality Assurance & Testing. Presently, I am working part-time with <a href=\" https://vyorius.com/\" class=\"text-primary\">Vyorius</a> to deploy their monolith applications on AWS.<br><br>Here are my key skills in Machine Learning and Full-Stack development.",
+    "ml": "\
+        <li>Python (NumPy, Pandas, Scikit-Learn etc.)</li>\
+        <li>PyTorch / Keras</li>\
+        <li>Spark / Hadoop</li>\
+        <li>Natural Language Processing</li>\
+        <li>Predictive Modelling</li>\
+        <li>Unsupervised Learning </li>\
+        <li>Deep Learning</li>\
+    ",
+    "fs": "\
+        <li>Python / C / C++ / Java</li>\
+        <li>HTML / CSS / JS</li>\
+        <li>SQL / MongoDB / DynamoDB</li>\
+        <li>Node.js / Flask / FastAPI</li>\
+        <li>React / JQuery / AngularJS</li>\
+        <li>Amazon Web Services (EC2, Lambda, Cognito, Kinesis etc.)</li>\
+        <li>Elasticsearch</li>\
+    "
+  },
+  "experience": [
+    {
+      "name": "New York University - Tandon School of Engineering",
+      "role": "Graduate Research Assistant",
+      "type": "Part Time",
+      "duration": "Dec 2020 - Present",
+      "description": "\
+        <li>Building data pipelines to process, visualize, and model the vast amounts of data produced by the various study programs taught at NYU Tandon Bridge</li>\
+        <li>Modelling knowledge graphs to map the various topics taught in the program and identify connectivity amongst them</li>\
+      "
+    },
+    {
+      "name": "Vyorius LLP",
+      "role": "Product Lead",
+      "type": "Part-time",
+      "duration": "Mar 2020 - Present",
+      "description": "\
+      <li>Deployed the Vyorius Control Center (minimum viable product) for the drone operators (end users) on AWS</li>\
+      <li>Converting the monolithic architecture of the product to microservices oriented architecture on AWS (utilizing EC2, S3, DynamoDB, Lambda, SNS, SQS, Code Pipeline etc.)</li>\
+      <li>Building a minimal CICD pipeline with Travis CI for continuous integration on GitHub and Code Deploy for continuous deployment on AWS</li>\
+      <li>Leading the software development and AI development teams for building various features and integrating them into dashboards for the Vyorius MVP</li>\
+    "
+    },
+    {
+      "name": "New York University - Center for Data Science",
+      "role": "Graduate Research Assistant",
+      "type": "Part-time",
+      "duration": "Jan 2020 - Jun 2020",
+      "description": "\
+      <li>Developed an open-source Python library called FairPrep to design and evaluate machine learning models for social fairness and enable the users to model human demographics data without any bias (based on race, gender, orientation etc.)</li>\
+      <li>Contributed towards the development of the FairPrep open-source package for Python.</li>\
+    "
+    },
+    {
+      "name": "Infosys Limited",
+      "role": "System Engineer",
+      "type": "Full-time",
+      "duration": "Oct 2017 - May 2019",
+      "description": "\
+      <li>Deployed ML solutions to full-stack application (MEAN) and designed D3.JS visualizations for SQA processes such as test-suite optimization, defect analysis, and requirement-testcase traceability</li>\
+      <li>Achieved up to 30% test suite optimization (reduction) by identifying the redundant or similar test cases and it resulted in an average of 10-15% effort savings per build cycle/sprint in regression testing for the clients</li>\
+      <li>Analyzed the test suite optimization model (uses hierarchical clustering) for presenting it as an explainable AI solution to the clients</li>\
+      <li>Augmented the traceability functionality by improving the simple bag-of-words based lexical similarity model to a semantic similarity model and improved the minimum mapping accuracy from 10% to 35% in an unsupervised learning model</li>\
+      <li>Platform being used by leading Infosys projects in Financial Services and Energy domain</li>\
+    "
+    },
+  ]
 };
--- js/script.js
+++ js/script.js
@@ -2,11 +2,43 @@
 
 $(document).ready(function(){
   // 1. About
-  $("#about-p1").append(data.about.intro)
-  $("#about-p2").append(data.about.end)
-  $("#about-ml").append(data.about.techs)
-  $("#about-fs").append(data.about.skills)
+  $("#about-p1").html(data.about.intro);
+  // Default skill
+  $("#about-skill").html(data.about.ml);
+  // Listener for skills
+  $('input[name=skills]').change(function () {
+    let curr = $('input[name=skills]:checked').val();
+    if (curr === '0'){
+      // console.log(curr)
+      $("#about-skill").html(data.about.ml);
+    }
+    else if (curr === '1'){
+      // console.log(curr)
+      $("#about-skill").html(data.about.fs);
+    }
+  });
+  
 
   // 2. Experience
+  // Default experience
+  let e = data.experience[0];
+  $("#exp-role").html(e.role);
+  $("#exp-name").html(e.name);
+  $("#exp-type").html(e.type);
+  $("#exp-duration").html(e.duration);
+  $("#exp-description").html(e.description);
+  // Listener for experience 
+  $('input[name=experience]').change(function () {
+    let curr = parseInt($('input[name=experience]:checked').val());
+    let e = data.experience[curr];
+    $("#exp-role").html(e.role);
+    $("#exp-name").html(e.name);
+    $("#exp-type").html(e.type);
+    $("#exp-duration").html(e.duration);
+    $("#exp-description").html(e.description);
+
+  });
+  
+
 
 });
