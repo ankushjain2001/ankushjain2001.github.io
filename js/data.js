af9a8a62516ddc38f9a2e6768ed44562681d40dd
Ankush Jain <ankushjain2001@yahoo.co.in>
2021-01-14 13:16:41 -0600

    UI changes

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@ var data = {
   "about": {
     "intro": "Hello! I am Ankush Jain. I am a machine learning engineer and a full-stack developer. My experience has taught me to apply my skills to real-world problems. And my on-going study in the master's program at <a href=\"https://engineering.nyu.edu/academics/programs/computer-science-ms\" class=\"text-primary\">New York University</a> has further equipped me with all the essential tools for my niche.<br><br>Previously, I have worked as a Software Engineer at <a href=\"https://www.infosys.com/services/validation-solutions/service-offerings/machine-learning-qa.html\" class=\"text-primary\">Infosys</a>. I worked on a variety of machine learning solutions for Software Quality Assurance & Testing. Presently, I am working part-time with <a href=\" https://vyorius.com/\" class=\"text-primary\">Vyorius</a> to deploy their monolith applications on AWS.<br><br>Here are my key skills in Machine Learning and Full-Stack development.",
     "ml": "\
-        <li>Python (NumPy, Pandas, Scikit-Learn etc.)</li>\
+        <li>Python (NumPy, Pandas, Scikit-Learn etc.) / R</li>\
         <li>PyTorch / Keras</li>\
         <li>Spark / Hadoop</li>\
         <li>Natural Language Processing</li>\
